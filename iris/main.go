90d819b73134b23a02a49046d8e9ffbc9aa7b4f5
James Li <muzinan123@users.noreply.github.com>
2024-09-26 13:21:08 +0800

    Update main.go

--- iris/main.go
+++ iris/main.go
@@ -10,7 +10,6 @@ func main()  {
 	app:=iris.New()
 	app.Logger().SetLevel("debug")
 	app.RegisterView(iris.HTML("./web/views",".html"))
-	//注册控制器
 	mvc.New(app.Party("/hello")).Handle(new(controllers.MovieController))
 	app.Run(
 		iris.Addr("localhost:8080"),
